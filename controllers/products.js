4aa3d16c8503caa425577ecba386ef5441290fe0
anna <anna!osher@gmail.com>
2022-05-18 20:42:50 +0200

    llast updata

--- controllers/products.js
+++ controllers/products.js
@@ -1,30 +1,34 @@
 const database = require("./database");
 
 module.exports = {
-  // addProduct: function (name, desc, price) {
-  //     if (!name || name.length === 0) {
-  //         throw ('ERROR: name is empty');
-  //     }
+  // todo: update to use resent changes...
 
-  //     database.pool.getConnection(function (connErr, connection) {
-  //         if (connErr) throw connErr; // not connected!
+  addProduct: async function (req, res, next) {
+    //     if (!name || name.length === 0) {
+    //         throw ('ERROR: name is empty');
+    //     }
 
-  //         const sql = "INSERT INTO products(name, description, price)" +
-  //             " VALUES(?,?,?);";
+    //     database.pool.getConnection(function (connErr, connection) {
+    //         if (connErr) throw connErr; // not connected!
 
-  //         connection.query(
-  //             sql,
-  //             [name, desc, price],
-  //             function (sqlErr, result, fields) {
-  //                 if (sqlErr) throw sqlErr;
+    const sql =
+      "INSERT INTO products(name, description, price)" + " VALUES(?,?,?);";
 
-  //                 console.log(result);
-  //             });
-  //     });
-  // },
+    //         connection.query(
+    //             sql,
+    //             [name, desc, price],
+    //             function (sqlErr, result, fields) {
+    //                 if (sqlErr) throw sqlErr;
+
+    //                 console.log(result);
+    //             });
+    //     });
+
+    res.send("todo add products");
+  },
 
   productsList: async function (req, res, next) {
-    const sql = "SELECT * FROM products";
+    const sql = "SELECT * FROM products ORDER BY name ASC;";
 
     try {
       // const connection = await database.getConnection();
@@ -34,4 +38,31 @@ module.exports = {
       console.log(err);
     }
   },
+
+  // todo: search product by name
+  exportProducts: async function (req, res, next) {
+    const sql =
+      "SELECT name,description,price FROM products ORDER BY name ASC;";
+    res.send("todo export products");
+  },
+
+  // todo: edit product details
+  editProduct: async function (req, res, next) {
+    // const sql = UPDATE
+    res.send("todo update products");
+  },
+
+  // todo: delete product
+  deleteProduct: async function (req, res, next) {
+    // const sql = DROP
+    res.send("todo delete products");
+  },
+
+  // todo: search product by name
+  searchProducts: async function (req, res, next) {
+    // const sql = SELECT WHERE...
+    res.send("todo search products");
+  },
+
+  // todo: sort products by name...
 };
